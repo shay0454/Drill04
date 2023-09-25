4034c10023789f2604e6170740ae94d07033008b
shay0454 <108744131+shay0454@users.noreply.github.com>
2023-09-25 23:58:21 +0900

    add handle_event, set intial variable

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -1,8 +1,16 @@
 import os
 from pico2d import *
 os.chdir("C:\\github\\2Dprogramming\\Drill04")
+TUK_WIDTH,TUK_HEIGHT=1280,1024
+ch_size=[32,32]
+running =True
+x,y=0,0
+frame=0
+def handle_event():
+    pass
+open_canvas(TUK_WIDTH,TUK_HEIGHT)
 character=load_image("character_v2.png")
 TUK_GROUND=load_image("TUK_GROUND.png")
-TUK_WIDTH,TUK_HEIGHT=1280,1024
-open_canvas(TUK_WIDTH,TUK_HEIGHT)
+
+
 close_canvas()
