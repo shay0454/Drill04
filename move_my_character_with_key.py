47e5294cd0d23f40291303b433ad621ba5df3046
shay0454 <108744131+shay0454@users.noreply.github.com>
2023-09-25 21:20:19 +0900

    add init_py

---
+++ move_my_character_with_key.py
@@ -0,0 +1,7 @@
+import os
+from pico2d import *
+os.chdir("C:\\github\\2Dprogramming\\Drill04")
+character=load_image("character_v2.png")
+TUK_GROUND=load_image("TUK_GROUND.png")
+open_canvas()
+close_canvas()
