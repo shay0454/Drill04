91950d0c6ba64414702e2bb51d04cbcc4ca74f36
shay0454 <108744131+shay0454@users.noreply.github.com>
2023-09-25 21:24:54 +0900

    set canvas's width, height

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -3,5 +3,6 @@ from pico2d import *
 os.chdir("C:\\github\\2Dprogramming\\Drill04")
 character=load_image("character_v2.png")
 TUK_GROUND=load_image("TUK_GROUND.png")
-open_canvas()
+TUK_WIDTH,TUK_HEIGHT=1280,1024
+open_canvas(TUK_WIDTH,TUK_HEIGHT)
 close_canvas()
